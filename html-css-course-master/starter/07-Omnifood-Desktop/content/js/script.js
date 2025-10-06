07b7d53ae8c7e5334cd57ed611c7908040c2e791
solomon54 <solomontsehay54@gmail.com>
2025-10-06 13:07:02 +0300

    Enhances navigation with smooth scrolling and updates section links for improved accessibility

--- html-css-course-master/starter/07-Omnifood-Desktop/content/js/script.js
+++ html-css-course-master/starter/07-Omnifood-Desktop/content/js/script.js
@@ -18,3 +18,34 @@ navElclose.addEventListener('click',function(){
     headerEl.classList.remove('nav--open');
 });
 
+
+////////////////////////////////////////////////////////////////////
+//////      Smooth scrolling animation    ///////////////////////////////
+////////////////////////////////////////////////////////////////////
+const allLinks=document.querySelectorAll('a:link');
+allLinks.forEach(function (link){
+    link.addEventListener('click',function(e){
+        e.preventDefault();
+        const href=link.getAttribute('href');
+        if(href==='#'){
+            window.scrollTo({
+                top:0,
+                behavior: 'smooth'
+            })
+        }
+
+        // scroll to other links
+        if(href!=="#"&& href.startsWith('#')){
+            const sectionEl=document.querySelector(href);
+            sectionEl.scrollIntoView({behavior:'smooth'})
+        }
+
+
+        // close mobile navigation
+        if(link.classList.contains('navigation--link'))
+    headerEl.classList.toggle('nav--open');
+
+
+    });
+    
+});
